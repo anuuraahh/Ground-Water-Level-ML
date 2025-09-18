4fe33c9191a08b8b7d0097761fc87ad9535b3ac0
anuuraahh <artmk2006@gmail.com>
2025-09-18 12:12:14 +0530

    initial commit

--- RandomForestRegressor_Ground_Water.py
+++ RandomForestRegressor_Ground_Water.py
@@ -1,39 +1,44 @@
 import pandas as pd
 import numpy as np
-import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+import joblib
 
 df = pd.read_csv(r'C:\Users\Murali\Downloads\kinathukadavu_ml_ready.csv')
 df['datetime'] = pd.to_datetime(df['datetime'], format='%d-%m-%Y %H:%M')
 df = df.dropna()
 
+df = df[(df['water_level_m'] > -50) & (df['water_level_m'] < 50)]
+
+for lag in range(1,6):
+    df[f'water_level_m_lag{lag}'] = df['water_level_m'].shift(lag)
+df = df.dropna()
+
 X = df.drop(columns=['datetime', 'water_level_m'])
 y = df['water_level_m']
+df = df.dropna()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+train_size = int(len(df) * 0.8)
+X_train, X_test = X.iloc[:train_size], X.iloc[train_size:]
+Y_train, y_test = y.iloc[:train_size], y.iloc[train_size:]
 
-model = RandomForestRegressor()
-model.fit(X_train, y_train)
+model = RandomForestRegressor(random_state=42)
+model.fit(X_train, Y_train)
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 r2 = r2_score(y_test, y_pred)
 
-print("GROUND WATER PREDICTION ANALYSIS USING RANDOM FOREST REGRESSION \n")
+print(f"GROUND WATER PREDICTIVE ANALYSIS USING RANDOM FOREST REGRESSION: ")
 print(f'Root Mean Squared Error: {rmse:.4f}')
 print(f'Mean Squared Error: {mse:.4f}')
 print(f'R^2 Value: {r2:.4f}')
 
-joblib.dump(model, "groundwater_model.pkl")
-print("Model saved.")
-results = pd.DataFrame({
-    "Actual": y_test.values,
-    "Predicted": y_pred
-})
-results.to_csv("groundwater_predictions.csv", index=False)
-print("Predictions saved.")
+joblib.dump(model, "groundwater_model_multilag.pkl")
 
+results = pd.DataFrame({"Actual": y_test.values, "Predicted": y_pred})
+results.to_csv("groundwater_predictions_multilag.csv", index=False)
+print("Predictions saved.")
 
