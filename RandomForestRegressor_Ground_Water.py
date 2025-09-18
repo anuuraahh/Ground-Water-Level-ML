2cbdcd470364278983c1b9fc7a957b1f9b7e29e8
anuuraahh <artmk2006@gmail.com>
2025-09-18 09:53:22 +0530

    initial commit

--- RandomForestRegressor_Ground_Water.py
+++ RandomForestRegressor_Ground_Water.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 df = pd.read_csv(r'C:\Users\Murali\Downloads\kinathukadavu_ml_ready.csv')
@@ -14,7 +14,7 @@ y = df['water_level_m']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LinearRegression()
+model = RandomForestRegressor()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
@@ -23,7 +23,7 @@ mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 r2 = r2_score(y_test, y_pred)
 
-print("GROUND WATER PREDICTION ANALYSIS USING LINEAR REGRESSION \n")
+print("GROUND WATER PREDICTION ANALYSIS USING RANDOM FOREST REGRESSION \n")
 print(f'Root Mean Squared Error: {rmse:.4f}')
 print(f'Mean Squared Error: {mse:.4f}')
 print(f'R^2 Value: {r2:.4f}')
